64ab4ee90ab320fefc6b0748f1b994c109cd1409
iveaaaa <davenaivea22@gmail.com>
2026-06-13 22:38:47 +0800

    Clear MAIL_PASSWORD in app configuration
    Remove sensitive information for email password.

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config['MAIL_SERVER']   = 'smtp.gmail.com'
 app.config['MAIL_PORT']     = 587
 app.config['MAIL_USE_TLS']  = True
 app.config['MAIL_USERNAME'] = 'uniattendfyp@gmail.com'
-app.config['MAIL_PASSWORD'] = 'rwii tzva tvsp xkrk'
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 
